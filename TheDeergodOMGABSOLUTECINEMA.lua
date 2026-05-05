4bac79751a4dbe6f56d1b65c8038f2edaf97aaf0
ItzzGabbbb <gabriel.gulberti.12@gmail.com>
2026-05-05 16:17:04 +0200

    Add files via upload

---
+++ TheDeergodOMGABSOLUTECINEMA.lua
@@ -0,0 +1,93 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/DenDenZZZ/Kavo-UI-Library/main/Kavo.lua"))()
+ 
+local Window = Library.CreateLib("Doors modes and mods", "DarkTheme")
+ 
+local Tab = Window:NewTab("mods")
+ 
+local Section = Tab:NewSection("have fun")
+ 
+Section:NewButton("hardcore mode by DripCapybara", "fix version", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DripCapybara/Doors-Mode-Remakes/refs/heads/main/HardcoreFixed.lua"))()
+end)
+ 
+Section:NewButton("Endless mode fixed", "by DripCapybara", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DripCapybara/Doors-Mode-Remakes/refs/heads/main/EndlessMode.lua"))()
+end)
+ 
+Section:NewButton("plamen6789 hardcore", "fixed", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/F9h0qzWW"))()
+end)
+ 
+Section:NewButton("Mayhem Mode", "by localplayerr", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/localplayerr/Doors-stuff/refs/heads/main/Mayhem%20mode%20recreate/Code"))()
+end)
+ 
+Section:NewButton("hardcore remastered", "ButtonInfo", function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/daiayday/HardcoreRemastered/refs/heads/main/obf_L6zO2N0sG5ehDHd56JBc5z70TZ2piVOvIqsgE88741HXGrwv7E4qd6c8axu8z4oh.lua.txt"))()
+end)
+ 
+Section:NewButton("hardcore old", "by localplayerr", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/localplayerr/Doors-stuff/refs/heads/main/Hardcore%20v4%20recreate/main%20code"))()
+end)
+ 
+Section:NewButton("Darkness v2", "ButtonInfo", function()
+    loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\34\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\83\99\114\105\112\116\53\48\51\57\51\57\50\47\68\97\114\107\110\101\115\115\77\111\100\101\47\109\97\105\110\47\83\99\114\105\112\116\47\76\111\97\100\101\114\46\108\117\97\34\41\41\40\41")()
+end)
+ 
+Section:NewButton("Impossible mode", "ButtonInfo", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Ukazix/impossible-mode/main/Protected_79.lua.txt'))()
+end)
+ 
+Section:NewButton("hardcore remake", "by Jay", function()
+    loadstring(game:HttpGet("https://glot.io/snippets/gp5pu59o7f/raw"))()
+end)
+ 
+Section:NewButton("kodbolx nightmare mode", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/kodbolx/kodbolhub/refs/heads/main/Nightmare%20Mode/Nightmare%20Mode.lua"))()
+end)
+ 
+Section:NewButton("gace mode", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/IdkMyNameLoll/PublicScripts/refs/heads/main/GraceEntitiesScript"))()
+end)
+ 
+local Tab = Window:NewTab("Extra")
+ 
+local Section = Tab:NewSection("very cool things")
+ 
+Section:NewButton("Guiding Candle ", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ChronoAcceleration/Comet-Development/refs/heads/main/Doors/Assets/Storage/Candle.lua"))()
+end)
+ 
+Section:NewButton("smiler spawner", "ButtonInfo", function()
+    loadstring(game:HttpGet('https://pastefy.app/YMa2CPBC/raw'))()
+ end)
+ 
+ Section:NewButton("golden Flashlight gummy", "ButtonInfo", function()
+   loadstring(game:HttpGet(("https://raw.githubusercontent.com/aadyian9000/the-thing/main/GoldenGummyFlashlight.lua"),true))()
+end)
+ 
+Section:NewButton("kodbolhub", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/kodbolx/kodbolhub/refs/heads/main/Kodbol%20Hub/Main.lua"))()
+end)
+ 
+Section:NewButton("Script mode (UPDATE)", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Munciseekawa/Sc/refs/heads/main/ScrtptsMode"))()
+end)
+ 
+Section:NewButton("crucifix", "by PenguinManiack", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/PenguinManiack/Crucifix/main/Crucifix.lua'))()
+end)
+ 
+Section:NewButton("hardcore a-60 spawner", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Idk-lol2/a-60aa/refs/heads/main/---%3D%3D%3D%3D%3D%3D%20a-60%20agresiv%20spawner%20%3D%3D%3D%3D%3D%3D---.txt"))()
+end)
+ 
+Section:NewButton("Entity spawner", "ButtonInfo", function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/plamen6789/UtilitiesHub/main/UtilitiesGUI'))()
+end)
+ 
+local Tab = Window:NewTab("Credits")
+ 
+local Section = Tab:NewSection("Normal")
+local Section = Tab:NewSection("hub by greed (me")
+local Section = Tab:NewSection("Credits to everybody")
